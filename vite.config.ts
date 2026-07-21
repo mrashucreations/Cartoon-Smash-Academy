26d8ae45f6078fe00af5981af994b9901ce62f75
mrashucreations <mrashucreations@gmail.com>
2026-07-21 13:50:41 +0530

    build: configure GitHub Pages deployment
    Add base path configuration to Vite and implement a CI/CD workflow to automate deployment to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Cartoon-Smash-Academy/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
